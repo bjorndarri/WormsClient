a2c82870a9f21f167089a4401e4eedff47dbdbe1
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2026-01-06 14:32:48 +0000

    Upgrade to JDK 17 with JPMS module support
          - Add module-info.java with proper module dependencies
          - Upgrade maven-compiler-plugin to 3.13.0 with release=17
          - Add maven-surefire-plugin 3.5.2 for JUnit 6 support
          - Upgrade to JUnit 6

--- src/test/java/de/uni_jena/cs/fusion/client/worms/WormsClientTest.java
+++ src/test/java/de/uni_jena/cs/fusion/client/worms/WormsClientTest.java
@@ -9,9 +9,9 @@ package de.uni_jena.cs.fusion.client.worms;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,8 +20,7 @@ package de.uni_jena.cs.fusion.client.worms;
  * #L%
  */
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -30,7 +29,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author Jan Martin Keil
